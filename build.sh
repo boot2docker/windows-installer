48a6407b655b5d73104a2dca9da971caf9e571d4
Sven Dowideit <SvenDowideit@home.org.au>
2014-05-21 15:41:16 +1000

    add a better Icon, and put the  on the desktop. Tweak the vbox&msys installs to be quieter

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@ cd VirtualBox
 
 #http://www.virtualbox.org/manual/ch02.html
 #VirtualBox-4.3.10-93012-Win.exe -extract -silent -path .
+#http://www.catonrug.net/2013/03/virtualbox-silent-install-store-oracle-certificate.html
 
 cd ../Boot2Docker
 
-curl -o boot2docker.iso https://github.com/boot2docker/boot2docker/releases/download/v0.9.1/boot2docker.iso
+curl -L -o boot2docker.iso https://github.com/boot2docker/boot2docker/releases/download/v0.9.1/boot2docker.iso
 
-curl -o boot2docker.exe https://github.com/boot2docker/boot2docker-cli/releases/download/v0.9.2/boot2docker-v0.9.2-windows-amd64.exe
+curl -L -o boot2docker.exe https://github.com/boot2docker/boot2docker-cli/releases/download/v0.9.2/boot2docker-v0.9.2-windows-amd64.exe
--- start.sh
+++ start.sh
@@ -1,8 +1,17 @@
+#!sh
+ISO="$USERPROFILE/.boot2docker/boot2docker.iso"
+
+if [ ! -e "$ISO" ]; then
+	echo "copying initial boot2docker.iso (run 'boot2docker.exe download' to update"
+	mkdir -p "$USERPROFILE/.boot2docker"
+	cp boot2docker.iso "$ISO"
+fi
+
 echo "initialising..."
 ./boot2docker.exe init
 echo "starting..."
 ./boot2docker.exe start
-echo "connecting... (password: tcuser)"
+echo "connecting..."
 ./boot2docker.exe ssh
 
 read
